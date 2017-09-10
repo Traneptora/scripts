26a3c56c2a6078a7c32da2afee08b52600d8e473
Leo Izen <leo.izen@gmail.com>
2017-09-09 22:02:51 -0400

    mpv-launcher: Add mpv-launcher wrapper c script for unique process names in discord's game recognition

---
+++ src/mpv-launcher.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <tbz_shell_escape.h>
+
+int main(int argc, char *const argv[]){
+	printf("Playing %s...\n", argv[0]);
+	char *command = tbz_escape_argv("mpv", argc, argv);
+	return system(command);
+}
+
