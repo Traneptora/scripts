4498d29f0010e19a181eaf3147914ac22bbf923a
Leo Izen <leo.izen@gmail.com>
2025-02-14 15:29:22 -0500

    removed weird shell launcher stuff

--- src/c/tbz_shell_escape.h
+++
@@ -1,7 +0,0 @@
-#ifndef TBZ_SHELL_ESCAPE_H_
-#define TBZ_SHELL_ESCAPE_H_
-
-char *tbz_shell_escape(const char *arg);
-char *tbz_escape_argv(const char *argv0, int argc, char *const argv[]);
-
-#endif
